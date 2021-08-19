b84540110c1cb28ddd16e1011563afd0cef53884
antonio.martin <antonio.martin@schibsted.com>
2021-08-20 00:17:08 +0200

    moviing repository

--- src/main/kotlin/com/amm/poc/blog/infrastructure/framework/configuration/UserConfiguration.kt
+++ src/main/kotlin/com/amm/poc/blog/infrastructure/framework/configuration/UserConfiguration.kt
@@ -2,7 +2,7 @@ package com.amm.poc.blog.infrastructure.framework.configuration
 
 import com.amm.poc.blog.application.user.get.service.GetUserService
 import com.amm.poc.blog.application.user.save.service.SaveUserService
-import com.amm.poc.blog.infrastructure.framework.user.get.repository.UserRepository
+import com.amm.poc.blog.infrastructure.framework.user.repository.UserRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import com.amm.poc.blog.domain.user.repository.UserRepository as IUserRepository
