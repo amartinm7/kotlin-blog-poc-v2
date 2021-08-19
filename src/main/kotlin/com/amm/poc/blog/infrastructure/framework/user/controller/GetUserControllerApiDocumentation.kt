52cde6c2fdedf709d26dcbb587800d65aa003466
antonio.martin <antonio.martin@schibsted.com>
2021-08-19 21:38:32 +0200

    adding swagger tags for responses

--- src/main/kotlin/com/amm/poc/blog/infrastructure/framework/user/controller/GetUserControllerApiDocumentation.kt
+++ src/main/kotlin/com/amm/poc/blog/infrastructure/framework/user/controller/GetUserControllerApiDocumentation.kt
@@ -2,12 +2,21 @@ package com.amm.poc.blog.infrastructure.framework.user.controller
 
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 
 interface GetUserControllerApiDocumentation {
 
     @ApiOperation(value = "get user by id", notes = "get user by id and year as query param")
+    @ApiResponses(
+        value = [
+            ApiResponse(code = 200, message = "Successfully retrieved users"),
+            ApiResponse(code = 400, message = "Bad request"),
+            ApiResponse(code = 500, message = "You broke the server..")
+        ]
+    )
     fun process(
         @ApiParam(value = "id", required = true, example = "1")
         @PathVariable id: String,
